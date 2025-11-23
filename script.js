c43ffb270b1e05a2d60e8807c3abd1de2e2c03ab
Mahesh Singh <maheshcodelab@gmail.com>
2025-11-24 00:23:37 +0530

    lv2

--- script.js
+++ script.js
@@ -1,99 +1,121 @@
-// 1. Write a function `sayHello()` that prints `"Hello JavaScript"`.
+// 1. Write a higher-order function `runTwice(fn)` that
+// takes another function and executes it two times.
 
-// function sayHello(){
-//     console.log("hello JS")
-// };
-
-// sayHello();
-
-// 2. Create a function `add(a, b)` that returns their sum and log the result.
-
-// function add(a, b) {
-//     return a + b;
+// function runTwice(fn) {
+//     fn();
+//     fn();
 // }
 
-// let sum = (add(2, 5));
-// console.log(sum);
+// runTwice(function () {
+//     console.log("Hello HoF");
+// })
 
 
-// 3. Write a function with a default parameter `name = "Guest"` that prints `"Hi <name>"`.
+// 2. Create one pure function that always returns the
+// same output for a given input, and one impure
+// function using a global variable.
 
-// function guest(guest = "Guest"){
-//     console.log(`Hi ${guest}`)
+// function pure(a,b){
+//     console.log(a+b);
 // }
+// pure(10,20);
 
-// guest("Mahesh");
-
-
-// 4. Use rest parameters to make a function that adds unlimited numbers.
-
-// function addUntd(...val){
-//     // let sum = 0;
-//     // val.forEach(function(nums){
-//     //     sum += nums;
-//     // })
-
-//     // for (let i=0; i<val.length; i++){
-//     //     sum = sum + val[i];
-//     // }
-//     // console.log(sum);
-
-//     // let ans = val.reduce(function(acc, num){
-//     //     return acc + num;
-//     // },0);
-    
-//     // console.log(ans);
+// let global = 0;
+// function impure(a){
+//     global++;
+//     console.log(a+global);
 // }
+// impure(5);
+// impure(5);
 
-// addUntd(1,2,3,4,5,6,7,8,9);
+// 3. Write a function that uses object destructuring
+// inside parameters to extract and print `name` and
+// `age`.
 
-// 5. Create an IIFE that prints `"I run instantly!"`.
-
-// (function(){
-//  console.log("I run instantly!");
-// })();
-
-// 6. Make a nested function where the inner one prints a variable from the outer one.
-
-// function first(){
-//     let a = 10;
-//     function second(){
-//         console.log(a);
-//     }
-//     second();
+// function dest({name,age}){
+//         console.log(name, age);
 // }
+// dest({name: "mahesh", age: 20});
 
-// first();
-
-// 7. Create an array of 5 fruits. Add one at the end and remove one from the beginning.
-
-// let arr = ["Mango", "Apple", "Banana", "Orange", "Pineapple"]
-
-// arr.push("Grapes");
-// arr.unshift("Strawberry");
-
-// 8. Use a `for` loop to print all elements of an array.
-
-// let arr = [1,2,3,4,5,6,7,8];
-
-// for ( let i = 0; i<arr.length; i++){
-//     console.log(arr[i]);
-// }
-
-// 9. Create an object `person` with keys `name`, `age`, and `city`, and print each key’s value.
+// 4. Demonstrate the difference between normal
+// function and arrow function when used as object
+// methods (the `this` issue).
 
 // let obj = {
 //     name: "mahesh",
-//     age: 20,
-//     city: "Umaria",
-// };
+//     fnc: function(){
+//         console.log(this);
+//         let fnc2 = ()=>{
+//             console.log(this);
+//         }
+//         fnc2();
+//     },
+//    }
+// obj.fnc();
 
-// for ( let key in obj){
-//     console.log(`${key} :`, obj[key]);
+// 5. Given an array of numbers, use `map()` to create a
+// new array where each number is squared.
+
+// let arr = [2,4,6,8,10];
+// let newarr = arr.map(function (val){
+//  return(val+val);
+// })
+// console.log(newarr);
+
+// 6. Use `filter()` to get only even numbers from an
+// array.
+
+// let arr = [1,2,3,4,5,6,7,8,9];
+// let newarr = arr.filter(function(val){
+//     return val%2 == 0 ;
+// })
+// console.log(newarr);
+
+// 7. Use `reduce()` to find the total salary from an array
+// of numbers `[1000, 2000, 3000]`.
+
+// let arr = [1000, 2000, 2002];
+// let total = arr.reduce(function (acc, val){
+//     return acc + val;
+// }, 0);
+// console.log(total);
+
+// 8. Create an array of names and use `some()` and
+// `every()` to test a condition (e.g., all names longer than
+// 3 chars).
+
+// arr = [ "raj", "om ", "kashi", "yug"];
+// let ans = arr.every(function(val){
+//     return val.length > 3 ;
+// })
+// console.log(ans);
+
+// 9. Create an object `user` and test the behavior of
+// `Object.freeze()` and `Object.seal()` by
+// adding/changing keys.
+
+// let user = {
+//     name: "raja",
+//     age: 20,
+//     email: "r@r.r",
 // }
 
-// 10. Use `setTimeout()` to log `"Time’s up!"` after 2 seconds.
+// Object.seal(user);
+// user.name = " Mahesh"
+// user.social="instagram"
 
-// setTimeout(function(){
-//     console.log("its 3 seconds late");
-// }, 3000);
+// 10. Create a nested object (`user → address → city`) and
+// access the city name inside it.
+
+// let obj = {
+//     user : {
+//         name: "mahesh",
+//         address: {
+//             city: "Umaria",
+//         }
+
+//     }
+// }
+
+// let {city} = obj.user.address;
+// console.log(obj.user.address.city);
