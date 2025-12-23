aff515608d0987db69ccec76e7c5ba5f7ec13219
Mahesh Singh <maheshcodelab@gmail.com>
2025-12-23 23:37:01 +0530

    virtual peano l48 task

--- script.js
+++ script.js
@@ -1,56 +1,39 @@
-let user = [
-    {
-        image: "https://images.unsplash.com/photo-1581833971358-2c8b550f87b3?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YW5pbWV8ZW58MHx8MHx8fDA%3D",
-        name: "Devis",
-        roles: "Fullstack Developer • UI Designer",
-        skills: "React • Node • MongoDB",
-        contactText: "Contact Me"
-    },
-    {
-        image: "https://images.unsplash.com/photo-1618336753974-aae8e04506aa?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8OHx8YW5pbWV8ZW58MHx8MHx8fDA%3D",
-        name: "Alex",
-        roles: "DevOps Engineer",
-        skills: "AWS • Docker • Kubernetes",
-        contactText: "Message"
-    },
-    {
-        image: "https://images.unsplash.com/photo-1578632767115-351597cf2477?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fGFuaW1lfGVufDB8fDB8fHww",
-        name: "James",
-        roles: "Digital Marketer • SEO Specialist",
-        skills: "SEO • SEM • Analytics",
-        contactText: "Work With Me"
-    },
-    {
-        image: "https://images.unsplash.com/photo-1716045168176-15d310a01dc0?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MzB8fGFuaW1lfGVufDB8fDB8fHww",
-        name: "Ethan",
-        roles: "Cybersecurity Analyst",
-        skills: "Network Security • Pen Testing",
-        contactText: "Contact"
-    },
+let sounds = [
+  "music/28.mp3",
+  "music/29.mp3",
+  "music/30.mp3",
+  "music/31.mp3",
+  "music/32.mp3",
+  "music/33.mp3",
+  "music/34.mp3",
+  "music/35.mp3",
+  "music/36.mp3",
+  "music/37.mp3",
+  "music/38.mp3",
+  "music/39.mp3"
 ];
 
-let val = ''
+let keys = document.querySelectorAll(".key");
 
-user.forEach(function (user) {
-    val = val + `<div class="card">
-        <div class="img-box">
-  <img class="card-img" src="${user.image}" alt="">
-</div>
+function playSound(key) {
+  let index = key.dataset.sound;
+  let audio = new Audio(sounds[index]);
+  audio.currentTime = 0;
+  audio.play();
+
+  key.classList.add("active");
+  setTimeout(() => {
+    key.classList.remove("active");
+  }, 120);
+}
 
 
-        <div class="card-content">
-            <h2 class="name">${user.name}</h2>
+keys.forEach(key => {
+  key.addEventListener("click", () => playSound(key));
+});
 
-            <p class="roles">${user.roles}</p>
 
-            <div class="tag">${user.skills}</div>
-
-            <button class="contact">${user.contactText}</button>
-        </div>
-
-    </div>` ;
-})
-
-let main = document.querySelector('main');
-
-main.innerHTML = val;
+document.addEventListener("keydown", (e) => {
+  let key = document.querySelector(`.key[data-key="${e.code}"]`);
+  if (key) playSound(key);
+});
